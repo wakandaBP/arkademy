00bb550903b7d50c993b631af92c795d65621745
Felix <damar.smitt11@gmail.com>
2020-03-21 10:11:54 +0700

    Answer if the 2nd question

---
+++ 2.php
@@ -0,0 +1,22 @@
+<?php
+
+$username = usernameCheck("useRnam");
+$password = passwordCheck("paS5word!");
+echo "Username : " . $username;
+echo "\nPassword : " . $password;
+
+function usernameCheck($username){
+    $status = "Tidak Cocok!";
+    if (strlen($username) < 8 AND strlen($username) > 4 AND strtolower($username) == $username){
+        $status = "Cocok!";
+    }
+    return $status;
+}
+
+function passwordCheck($pass){
+    $status = "Tidak Cocok!";
+    if (preg_match('/^(?=.*\d)(?=.*[@#\-_$%^&+=§!\?])(?=.*[a-z])(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{9}$/', $pass)){
+        $status = "Cocok!";
+    }
+    return $status;
+}
